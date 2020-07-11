24cbdb5078971d6a277e8efc7386597d6d74e274
Caio Jaime <kobashijaime@gmail.com>
2020-07-10 23:16:56 -0300

    Operation.

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,7 @@
 class CalcController {
 
     constructor(){
+        this._operation = [];
         this._locale = 'pt-br';
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -8,6 +9,7 @@ class CalcController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+
     }    
     // Clock seconds timer.
     initialize(){
@@ -26,9 +28,82 @@ class CalcController {
 
         events.split(',').forEach( event => {
                 element.addEventListener(event, fn,false);
-        })
+        });
 
     }
+
+    clearAll(){
+
+        this._operation = [];
+
+    }
+
+    clearEntry(){
+
+        this._operation.pop();
+    }
+
+    addOperation(value){
+
+        this._operation.push(value);
+
+    }
+
+    setError(){
+
+        this.displayCalc = "Error";
+         
+    }
+
+    execBtn(value){
+
+        switch (value) {
+
+            case 'ac':
+                this.clearAll();
+                 break;
+                 
+            case 'ce':
+                this.clearEntry();
+                 break;
+            case 'soma':
+
+                 break;
+            case 'divisao':
+
+                 break; 
+            case 'multiplicacao':
+                
+                 break;
+            case 'subtracao':
+                
+                  break;
+           case 'porcento':
+                
+                 break;
+           case 'igual':
+                
+                 break;
+
+            case '0':
+            case '1':     
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                 break; 
+
+            default:
+                this.setError();
+                 break;
+        }
+    }
+
     // Events.
     initButtonsEvents(){
 
@@ -38,8 +113,9 @@ class CalcController {
 
             this.addEventListenerAll(btn,"click, drag", e =>{
 
-                console.log(btn.className.baseVal.replace("btn-",""));
+                let textBtn = btn.className.baseVal.replace("btn-","");
                 
+                this.execBtn(textBtn);
             });
             // Changing pointer.
             this.addEventListenerAll(btn, "mouseover, mouseup, mousedown", e=>{
